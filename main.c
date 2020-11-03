0abf88be07e2e1ede83df53a5f43f03697e6d845
guija <guija@192.168.1.70>
2020-11-03 12:01:56 -0600

    Prueba de Hola Mundo

---
+++ main.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: guija
+ *
+ * Created on 2 de noviembre de 2020, 02:25 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main() {
+
+    printf("Holaaa mundo");
+    
+    return (EXIT_SUCCESS);
+}
+
