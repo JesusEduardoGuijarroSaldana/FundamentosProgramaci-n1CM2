80d64c0532a781be4bc4a884f53eef436b9548ba
guija <guija@192.168.1.70>
2020-11-08 14:57:00 -0600

    Operaciones (Clase3Nov2020)

--- main.c
+++ main.c
@@ -9,18 +9,50 @@
 #include <stdlib.h>
 
 int main() {
-    float Resuno = 0;
-    float Resdos = 0;
-    int Restres = 0;
+    /*Para el mayor de 3 números*/
+    int num1 = 50;
+    int num2 = 89;
+    int num3 =7;
+    /*Para el mayor de 4 números*/
     
-    Resuno = (6.18/5)*(((14.1-4)/2)*((14.1-4)/2));
-    Resdos = ((8.9*34)/17);
-    Restres = 8-7;
-    
-    printf ("El primer resultado es: " "%f",Resuno);
-    printf ("\nEl segundo resultado es: " "%f",Resdos);
-    printf ("\nEl tercer resultado es: " "%d",Restres);
+    int num4 = 69;
+    int num5 = 11;
+    int num6 = 200;
+    int num7 = 5;
   
+   
+    /*Para el mayot de 3 números*/
+    
+    if (num1 > num2 && num1 >num3 ){
+        printf ("El número mayor de los 3 es: " "%d", num1);
+    } 
+    else if (num2 > num1 && num2 > num3){
+        printf ("El número mayor de los 3 es: " "%d", num2);
+    }
+    else if (num3 > num1 && num3 > num2){
+        printf ("El número mayor de los 3 es: " "%d", num3);
+    }
+    else if (num1 == num2 && num1 == num3){
+        printf ("Todos los números son iguales");
+    }
+  
+    /*Para el mayor de 4 números*/
+    
+    if (num4 > num5 && num4 > num6 && num4 > num7){
+        printf ("\nEl número mayor de los 4 es: " "%d", num4);
+    }
+    else if (num5 > num4 && num5 > num6 && num5 > num7){
+        printf ("\nEl número mayor de los 4 es: " "%d", num5);     
+    }
+    else if (num6 > num4 && num6 > num5 && num6 > num7){
+        printf ("\nEl número mayor de los 4 es: " "%d", num6);
+    }
+    else if (num7 > num4 && num7 > num5 && num7 > num6){
+        printf ("\nEl número mayor de los 4 es: " "%d", num7);
+    }
+    else if (num4 == num5 && num4 == num6 && num4 == num7){
+        printf ("\nTodos los números son iguales");
+    }        
     return (EXIT_SUCCESS);
 }
 
