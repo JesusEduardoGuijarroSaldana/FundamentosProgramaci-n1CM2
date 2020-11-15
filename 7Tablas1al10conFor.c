2632371ead4fcb350e452099492d8cc0463d8c94
guija <guija@192.168.1.70>
2020-11-15 14:10:21 -0600

    Tablas del 1 al 10 con For

---
+++ 7Tablas1al10conFor.c
@@ -0,0 +1,30 @@
+/* 
+ * File:   7Tablas1al10conFor.c
+ * Author: guija
+ *
+ * Created on 15 de noviembre de 2020, 01:45 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv) {
+    int x;
+    int y;
+    
+    for (x=1;x<11;){
+        printf ("Tabla del %d\n", x);
+        
+        for (y=1;y<11;y = y +1){
+            printf ("%d X %d = %d\n", x, y, x*y);
+        
+        }
+    
+    x = x+1;
+    y = 1;
+    }
+
+    return (EXIT_SUCCESS);
+}
+
