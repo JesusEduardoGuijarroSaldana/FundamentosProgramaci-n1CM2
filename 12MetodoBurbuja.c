a59d163358900488998116099d437ab7117c8c4a
guija <guija@192.168.1.70>
2020-11-24 14:03:30 -0600

    Actualización

--- 12MetodoBurbuja.c
+++ 12MetodoBurbuja.c
@@ -12,35 +12,7 @@
  * 
  */
 int main(int argc, char** argv) {
-   
-    int x, y, cantidad, aux;
-    
-    
-    printf("\n¿Cuántos números desea evaluar?");
-    scanf("%i", &cantidad);
-    
-    int num[cantidad];
-    
-    for (y=0; y<cantidad; y=y+1){
-        printf("Digite el valor para arreglo [%i]: ",y);
-        scanf("%i", &num[y]);
-    }
 
-    for(x=0; x<cantidad; x=x+1){
-        for(y=0; y<cantidad; y=y+1){
-            if(num[y]>num[y+1]){
-                aux=num[y];
-                num[y]=num[y+1];
-                num[y+1]=aux;
-            }
-        }
-    }
-    
-    
-    for(x=0; x<cantidad; x=x+1){
-        printf ("\nOrdenado %i ", num[x]);
-    }
-    
     return (EXIT_SUCCESS);
 }
 
